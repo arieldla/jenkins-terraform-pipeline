bb3240ec997b7694575817f77b2c8223601ba895
arieldla <arieldla@me.com>
2026-03-17 19:39:59 -0400

    Update S3 bucket name in backend configuration

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "dla-tstate"
+    bucket  = "<terraform-state-bucket>"
     key     = "jenkins-pipeline/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
